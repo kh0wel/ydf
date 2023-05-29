4e2896c5b6800fdeae628bcdd90d0def32d4383f
kh0wel <the.kh0wel@gmail.com>
2023-05-29 17:42:34 -0400

    refac

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,5 @@
-import path from 'node:path';
-
-// import { exec } from 'node:child_process';
+import { resolve } from 'node:path';
+import { exec    } from 'node:child_process';
 
 import { Session } from '@biscuitland/core';
 
@@ -24,21 +23,21 @@ switch (process.argv.at(2)) {
 
     case 'init':
 
-        const name = process.argv.at(3) ?? 'new-yotrd-project';
+        exec(`git clone --depth=1 https://github.com/yotrd/example.git ${ process.argv.at(3) ?? 'new-yotrd-project' }`);
 
-        // exec(`git clone --depth=1 https://github.com/yotrd/template.git ${ name }; rm -rf ${ path.join(process.cwd(), name, '.git') }`);
+        console.log('Documentation on https://github.com/yotrd/core#readme');
 
         break;
 
     case 'deploy':
 
-        const { default: config } = await import(`file:///${ path.resolve(process.argv.at(3)) ?? path.join(process.cwd(), '.yotrd.config.js') }`);
+        const { default: config } = await import(`file:///${ resolve(process.argv.at(3) ?? (process.cwd(), '.yotrd.config.js')) }`);
 
-        const eventsPath                     = config.directories?.events            ?? path.join(process.cwd(), 'src', 'events');
-        const servicesPath                   = config.directories?.services          ?? path.join(process.cwd(), 'src', 'services');
-        const chatInputCommandsPath          = config.directories?.commands?.chat    ?? path.join(process.cwd(), 'src', 'commands', 'chat');
-        const userContextMenuCommandsPath    = config.directories?.commands?.user    ?? path.join(process.cwd(), 'src', 'commands', 'user');
-        const messageContextMenuCommandsPath = config.directories?.commands?.message ?? path.join(process.cwd(), 'src', 'commands', 'message');
+        const eventsPath                     = resolve(config.directories?.events            ?? (process.cwd(), 'src', 'events'));
+        const servicesPath                   = resolve(config.directories?.services          ?? (process.cwd(), 'src', 'services'));
+        const chatInputCommandsPath          = resolve(config.directories?.commands?.chat    ?? (process.cwd(), 'src', 'commands', 'chat'));
+        const userContextMenuCommandsPath    = resolve(config.directories?.commands?.user    ?? (process.cwd(), 'src', 'commands', 'user'));
+        const messageContextMenuCommandsPath = resolve(config.directories?.commands?.message ?? (process.cwd(), 'src', 'commands', 'message'));
 
         const loadedEvents                     = await getUsedFiles(eventsPath, EventBuilder);
         const loadedServices                   = await getUsedFiles(servicesPath, ServiceBuilder);
