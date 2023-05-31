3f19984f6a07645effee9f385b7861e4041acb7e
kh0wel <the.kh0wel@gmail.com>
2023-05-30 21:35:13 -0400

    refac

--- src/utils/loadFiles.js
+++ src/utils/loadFiles.js
@@ -1,28 +1,28 @@
-import { stat, readdir as readDirectory } from 'node:fs/promises';
-import { join          as joinPath      } from 'node:path';
+import fs from 'node:fs/promises';
+import path from 'node:path';
 
 async function loader (directory, Builder) {
 
     let loaded = [];
 
-    const items = (await readDirectory(directory, 'utf-8')).filter((name) => !name.startsWith('.'));
+    const items = (await fs.readdir(directory, 'utf-8')).filter((name) => !name.startsWith('.'));
 
     for (const item of items) {
 
-        const path = joinPath(directory, item);
+        const on = path.join(directory, item);
 
-        const { isDirectory } = await stat(path);
+        const { isDirectory } = await fs.stat(path);
 
         if (isDirectory()) {
 
-            loaded = loaded.concat(await loader(path, Builder));
+            loaded = loaded.concat(await loader(on, Builder));
 
             continue;
         };
 
         if (item.startsWith('main')) {
 
-            const { default: data } = await import(`file:///${ path }`);
+            const { default: data } = await import(`file:///${ on }`);
 
             loaded.push(new Builder({ ...data, name: item }));
 
