91efe3acd5909f3b5628ae10d8fc5a30a59f8367
kh0wel <the.kh0wel@gmail.com>
2023-06-04 16:16:34 -0400

    refac

--- src/loadFiles.js
+++ src/loadFiles.js
@@ -1,64 +1,99 @@
 import fs from 'node:fs/promises';
 import path from 'node:path';
 
+import { EventBuilder } from './structures/Event.js';
+import { ServiceBuilder } from './structures/Service.js';
+import { ChatInputCommandBuilder } from './structures/ChatInputCommand.js';
+import { UserContextMenuCommandBuilder } from './structures/UserContextMenuCommand.js';
+import { MessageContextMenuCommandBuilder } from './structures/MessageContextMenuCommand.js';
+
 export default async function (config) {
 
-    const laodedEvents                     = [];
-    const laodedServices                   = [];
-    const laodedChatInputCommands          = [];
+    const loadedEvents                     = [];
+    const loadedServices                   = [];
+    const loadedChatInputCommands          = [];
     const loadedUserContextMenuCommands    = [];
     const loadedMessageContextMenuCommands = [];
 
     async function loader (directory) {
 
-        let loaded = [];
-
         const items = (await fs.readdir(directory, 'utf-8')).filter((name) => !name.startsWith('.'));
 
         for (const item of items) {
-    
+
             const stat = await fs.stat(path.join(directory, item));
-    
+
             if (stat.isDirectory()) {
-    
-                loaded = loaded.concat(await loader(path.join(directory, item)));
-    
+
+                await loader(path.join(directory, item))
+
                 continue;
             }
-    
+
             if (item.endsWith()) {
-    
+
                 const { default: data } = await import(`file:///${ path.join(directory, item) }`);
-    
-                loaded.push(
-    
-                    new Builder({
-    
-                        ... data,
-    
-                        name: item.slice(0, item.length - target.length),
-    
-                        path: path.join(directory, item)
-                    })
-                );
-    
+
+                if (data instanceof EventBuilder) loadedEvents.push({
+
+                    ... data,
+
+                    name: item.slice(0, item.length - target.length),
+
+                    path: path.join(directory, item)
+                });
+
+                if (data instanceof ServiceBuilder) loadedServices.push({
+
+                    ... data,
+
+                    name: item.slice(0, item.length - target.length),
+
+                    path: path.join(directory, item)
+                });
+
+                if (data instanceof ChatInputCommandBuilder) loadedChatInputCommands.push({
+
+                    ... data,
+
+                    name: item.slice(0, item.length - target.length),
+
+                    path: path.join(directory, item)
+                });
+
+                if (data instanceof UserContextMenuCommandBuilder) loadedUserContextMenuCommands.push({
+
+                    ... data,
+
+                    name: item.slice(0, item.length - target.length),
+
+                    path: path.join(directory, item)
+                });
+
+                if (data instanceof MessageContextMenuCommandBuilder) loadedMessageContextMenuCommands.push({
+
+                    ... data,
+
+                    name: item.slice(0, item.length - target.length),
+
+                    path: path.join(directory, item)
+                });
+
                 break;
             }
         }
-
-        return loaded;
-    };
+    }
 
     for (const included of config.include) {
 
         await loader(path.resolve(included));
-    };
+    }
 
     return {
         
-        laodedEvents,
-        laodedServices,
-        laodedChatInputCommands,
+        loadedEvents,
+        loadedServices,
+        loadedChatInputCommands,
         loadedUserContextMenuCommands,
         loadedMessageContextMenuCommands
     };
