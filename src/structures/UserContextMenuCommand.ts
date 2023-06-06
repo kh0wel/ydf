487dc1b06d2f4cac9a7f71ea7d6e671862761f38
kh0wel <the.kh0wel@gmail.com>
2023-06-05 21:19:07 -0400

    refac

--- src/structures/UserContextMenuCommand.ts
+++ src/structures/UserContextMenuCommand.ts
@@ -1,30 +1,80 @@
+export interface UserContextMenuCommandOptions {
+
+    intents?: number;
+
+    display: {
+
+        name: {
+
+            default: string;
+
+            [locale: string]: string;
+        };
+
+        permissions?: {
+
+            member?: bigint | null;
+
+            dm?:   boolean;
+            nsfw?: boolean;
+        }
+    }
+
+    events: {
+
+        [event: string]: (parameters) => Promise<void> | void;
+    }
+}
+
 export class UserContextMenuCommandBuilder {
 
-    name; path; intents; display; events;
+    // https://discord.com/developers/docs/topics/gateway#gateway-intents
+    intents = 0; 
 
-    constructor (data) {
+    display: {
 
-        this.name = data.name;
-        this.path = data.path;
+        type: number;
 
-        // https://discord.com/developers/docs/topics/gateway#gateway-intents
-        this.intents = data.intents ?? 0;
+        name: {
 
-        this.display = {
+            default: string;
 
-            // https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-types
-            type: 2,
-
-            name: data.display.name,
-
-            permissions: {
-
-                member: data.display.permissions?.member ?? null,
-
-                dm:   data.display.permissions?.dm   ?? false,
-                nsfw: data.display.permissions?.nsfw ?? false
-            }
+            [locale: string]: string;
         };
+
+        permissions: {
+
+            member: bigint | null;
+
+            dm:   boolean;
+            nsfw: boolean;
+        };
+    } = {
+
+        // https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-types
+        type: 2,
+
+        name: { default: 'empty' },
+
+        permissions: {
+
+            member: null,
+
+            dm: false, nsfw: false
+        }
+    };
+
+    events = {};
+
+    constructor (data: UserContextMenuCommandOptions) {
+
+        this.intents = data.intents ?? this.intents;
+
+        this.display.name = data.display.name;
+
+        this.display.permissions.member = data.display.permissions?.member ?? this.display.permissions.member;
+        this.display.permissions.dm     = data.display.permissions?.dm     ?? this.display.permissions.dm;
+        this.display.permissions.nsfw   = data.display.permissions?.nsfw   ?? this.display.permissions.nsfw;
 
         this.events = data.events;
     }
